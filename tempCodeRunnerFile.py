65294fe021b6df0362bf8a944eea8224a8553a93
Katlicia <mama-4153@hotmail.com>
2024-10-26 16:16:46 +0300

    gitignore

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-                    unavailable_stock = product.find_element(By.CLASS_NAME, "add-to-basket.productNo.ng-star-inserted")
-                    if unavailable_stock.is_enabled():
-                        print(f"{product_name}: Out of stock")
-                        products[product_name] = 0
